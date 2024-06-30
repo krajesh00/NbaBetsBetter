45012ff178710653223f3d2ba80f7935733e5b0c
Fraser Morrison <frazemor@hotmail.com>
2024-06-29 20:35:48 -0400

    Added breakpoint input

--- nbabets/src/App.tsx
+++ nbabets/src/App.tsx
@@ -3,15 +3,17 @@ import "./App.css";
 import { getPlayers } from "./loader";
 import { PartialBet, Player } from "./model";
 import { PlayerSelector } from "./components/PlayerSelector";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
 
 function App() {
-
   const [players, setPlayers] = useState<Player[]>([]);
 
   const [bet, setBet] = useState<PartialBet>({});
 
-  // useEffect(() => console.log(bet), [bet]);
+  const [breakpoint, setBreakpoint] = useState<number>(0);
 
+  // useEffect(() => console.log(bet), [bet]);
 
   // Load player list on component mount
   useEffect(() => {
@@ -27,6 +29,14 @@ function App() {
           setSelectedPlayer={(p) => setBet((b) => ({ ...b, player: p }))}
         />
       </div>
+      <Label htmlFor="breakpoint">Breakpoint</Label>
+      <Input
+        id="breakpoint"
+        type="number"
+        value={breakpoint}
+        onChange={(e) => setBreakpoint(parseFloat(e.target.value))}
+        min={0}
+      />
     </>
   );
 }
