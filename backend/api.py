6688fce72213b65d711de8afcf9484570cdfe2df
krajesh00 <kishraj1807@gmail.com>
2024-07-08 23:36:23 -0400

    api-routes_V2

--- backend/api.py
+++ backend/api.py
@@ -19,15 +19,22 @@ async def get_players():
 @app.get("/players/{player_id}/{stat}/{lookback}")
 async def get_player_stat(player_id: int, stat: str, lookback: int):
     all_logs = pd.read_csv("all_data_logs.csv")
-    player_logs = all_logs[all_logs["player_id"] == player_id]
-    player_logs = player_logs[stat, 'GAME_DATE'].to_dict()
+    player_logs = all_logs[all_logs["PLAYER_ID"] == player_id]
+    player_logs = player_logs[[stat, 'GAME_DATE']].to_dict(orient='records')
     player_logs = player_logs[:lookback]
     return player_logs
+
+@app.get("/stats")
+async def get_available_stats():
+    all_logs = pd.read_csv("all_data_logs.csv")
+    columns = list(all_logs.columns)
+    columns = columns[10:]
+    return {"stats" : columns}
 
 @app.get("/statpredict/{player_id}/{stat}/{lookback}/{threshold}/{direction}/{over}/{under}")
 async def predict_stat(player_id: int, stat: str, lookback: int, threshold: float, over: float, under: float, direction: str):
     all_logs = pd.read_csv("all_data_logs.csv")
-    all_logs = all_logs[all_logs["player_id"] == player_id]
+    all_logs = all_logs[all_logs["PLAYER_ID"] == player_id]
     all_logs[stat] = pd.to_numeric(all_logs[stat], errors='coerce')
 
     # Select the most recent games
@@ -36,7 +43,7 @@ async def predict_stat(player_id: int, stat: str, lookback: int, threshold: floa
     # Calculate the number of games where the player exceeded the point threshold
     if direction == "over":
         successes = (recent_games[stat] > threshold).sum()
-    else:
+    elif direction == "under":
         successes = (recent_games[stat] < threshold).sum()
 
     successes = (recent_games[stat] > threshold).sum()
