03708a455b71b5429d62cff43e940dfcd1530d4b
krajesh00 <kishraj1807@gmail.com>
2024-07-11 22:04:05 -0400

    startup.sh-and-README.md

--- README.md
+++ README.md
@@ -11,12 +11,17 @@ docker-compose up
 ```
 
 To keep your data up to date, you will need to have prefect installed.
+Open a new terminal
+Make sure Python version 3.11 is downloaded
 You can install prefect with
 ```
-pip install prefect
+cd NbaBetsBetter
+source .venv/bin/activate
+sh startup.sh
 ```
+Then open a new terminal
 
-The data updating task can be run with:
 ```
-TODO: put prefect command here.
+cd dataflow
+python log_fill.py
 ```
--- startup.sh
+++ startup.sh
@@ -3,14 +3,13 @@
 prefect config set PREFECT_API_URL="http://127.0.0.1:4200/api"
 prefect server database reset -y 
 export PREFECT_API_SERVICES_FLOW_RUN_NOTIFICATIONS_ENABLED=false
-prefect start
 prefect config view --show-sources 
 
 read -p "Do you want to continue running the server? (y/n): " choice
 if [[ $choice == "y" ]]; then
     # Rest of your code goes here
     echo "Server is running..."
-    prefect server start & docker compose up
+    prefect server start
 
 else
     echo "Server stopped."
